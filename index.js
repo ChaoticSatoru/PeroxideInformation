d8b86ea0536a04635fa9e92381f778f7ef210364
ChaoticSatoru <foxtotally@gmail.com>
2024-10-14 20:55:36 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// Function to scroll to a specific section when a button is clicked
-function scrollToSection(sectionId) {
-    // Get the section by its ID
-    const section = document.getElementById(sectionId);
-
-    // Scroll to the section smoothly
-    section.scrollIntoView({ behavior: 'smooth' });
-}
-
